9bdb4db3cce6805c7abba7f72685f08aba23626b
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-27 17:57:06 +0545

    name length min 5 set

--- server/middleware/validateForm.js
+++ server/middleware/validateForm.js
@@ -2,7 +2,7 @@ const { body } = require('express-validator');
 
 exports.validateForm =
     [
-        body('name').not().isEmpty().isLength({ min: 5 }).withMessage('enter your name in correct format'),
+        body('name').isLength({ min: 5 }).not().isEmpty().withMessage('enter your name in correct format'),
         body('email').isEmail().not().isEmpty().withMessage('enter your email in correct format'),
         body('phone').isNumeric().isLength({ min: 10, max: 10 }).withMessage("please enter 10 number").not().isEmpty(),
         body('gender')
