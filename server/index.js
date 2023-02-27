7e9b607bcd234cecf79f9edc9c144c1d45cad969
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-27 17:42:21 +0545

    backend complete

--- server/index.js
+++ server/index.js
@@ -4,12 +4,11 @@ const formActions = require('./routes/formActions')
 const app = express()
 
 // middleware
-app.use(express.json())
-
+app.use(express.json());
 
 // routes
 app.use('/form', formActions)
 
-const PORT = process.env.PORT || 5000
 
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
