1f7dcf9569f9b0faeb83fe65ae5873144d00880c
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-27 16:08:27 +0545

    user table created

---
+++ server/tables/userTable.js
@@ -0,0 +1,22 @@
+const { client } = require('../config/db')
+
+const createUserTable = async () => {
+    try {
+        await client.query(`CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name varchar(100) NOT NULL,
+            email varchar(100) NOT NULL,
+            phone int NOT NULL,
+            gender varchar(100) NOT NULL,
+            hobbies text [] NOT NULL
+            );`)
+
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+
+createUserTable()
+    .then(() => console.log('User table created'))
+    .catch((err) => console.log(err))
