d687f15554cb84870662230ffed382a87b0ec1f7
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-27 17:27:31 +0545

    error handled

--- server/controller/formActions.js
+++ server/controller/formActions.js
@@ -1,6 +1,25 @@
-const { client } = require('../config/db')
+// const { client } = require('../config/db')
+const { validationResult } = require('express-validator')
 
 
 exports.submitForm = async (req, res) => {
-    res.send('Form submitted')
+    try {
+        const error = validationResult(req)
+
+        // if (error) {
+        //     return res.status(400).json({ message: error.array() })           // error is and array so even its empty it will return true
+        // }
+
+        // so we have to check if its empty or not
+        if (!error.isEmpty()) {
+            return res.status(400).json({ message: error.array() })
+        }
+
+        const { name, email, phone, gender, hobbies } = req.body;
+        res.status(400).json({ message: 'success' })
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+
+    }
 }
