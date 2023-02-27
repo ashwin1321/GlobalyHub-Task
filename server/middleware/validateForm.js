0550df1f3bded1965f01392bea2b63c298bda699
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-27 18:10:15 +0545

    validation error message added

--- server/middleware/validateForm.js
+++ server/middleware/validateForm.js
@@ -2,9 +2,9 @@ const { body } = require('express-validator');
 
 exports.validateForm =
     [
-        body('name').isLength({ min: 5 }).not().isEmpty().withMessage('enter your name in correct format'),
-        body('email').isEmail().not().isEmpty().withMessage('enter your email in correct format'),
-        body('phone').isNumeric().isLength({ min: 10, max: 10 }).withMessage("please enter 10 number").not().isEmpty(),
+        body('name').isLength({ min: 5 }).withMessage("name must be greater than 5 character").notEmpty().withMessage('name cannot be empty'),
+        body('email').notEmpty().withMessage("email cannot be empty").isEmail().withMessage('enter your email in correct format'),
+        body('phone').isNumeric().withMessage("number has to be a numeric value.").isLength({ min: 10, max: 10 }).withMessage("please enter 10 number").notEmpty().withMessage("number cannot be empty"),
         body('gender')
             .isIn(['male', 'female', 'others'])
             .withMessage('please select a option'),
